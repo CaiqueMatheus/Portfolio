b82a38030ec4681d72a45a1d44d04ee21a75c12a
Caique Matheus <dev.caique@gmail.com>
2021-04-26 16:54:58 -0300

    Add FontAwesome e Footer

--- portfolio/src/app/components/footer/footer.component.ts
+++ portfolio/src/app/components/footer/footer.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import { faBehanceSquare } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faArtstation } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +14,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  facebook = faFacebookSquare;
+  instagram = faInstagramSquare;
+  linkedin = faLinkedin;
+  behance = faBehanceSquare;
+  artstation = faArtstation;
+  github = faGithub;
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
   }
 
 }
