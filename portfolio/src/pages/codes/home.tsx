85624300501f8108b369834c80613355d3cd97c4
Caique Matheus <dev.caique@gmail.com>
2021-05-01 12:05:46 -0300

    Iniciada a página codes

--- portfolio/src/pages/codes/home.tsx
+++ portfolio/src/pages/codes/home.tsx
@@ -1,50 +1,57 @@
+import { GetStaticProps } from "next";
+import { api } from "../../services/api";
+import { format, parseISO } from "date-fns";
 import styles from "./home.module.scss";
 
-type user = {
+type User = {
   nickname: string;
   description: string;
   photo: string;
 };
 
-type member = {
+type Member = {
   nickname: string;
   photo: string;
 };
 
-type repository = {
+type Repository = {
   name: string;
   totalCommits: number;
 };
 
-type contribuition = {
+type Contribuition = {
   totalCommits: string;
-  topRepoList: repository[];
+  topRepoList: Repository[];
 };
 
-type working = {
+type WorkingCard = {
   projectName: string;
   platform: string;
   technologies: string[];
-  members: member[];
+  members: Member[];
   progress: string;
   timeEstimated: string;
   leftTime: string;
   public: boolean;
 };
 
-type profileCard = {
-  user: user;
+type ProfileCard = {
+  user: User;
   technologies: string[];
   repositories: number;
   commits: number;
-  contribuition: contribuition;
+  contribuition: Contribuition;
 };
 
-export default function home() {
+export default function home(ProfileCard, WorkingCard) {
   return (
     <div className={styles.container}>
       <section className={styles.board}>
-        <div className={styles.working}></div>
+        <div className={styles.working}>
+          <div className={styles.card}>
+            <h3></h3>
+          </div>
+        </div>
         <div className={styles.infos}>
           <div className={styles.commits}>
             <div className={styles.dayCommits}></div>
@@ -57,3 +64,36 @@ export default function home() {
     </div>
   );
 }
+/*
+export const getServerSideProps: GetStaticProps = async () => {
+  const { data } = await api.get("working-card", {
+    params: {
+      _limit: 4,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  let workingCards = data.map((card) => {
+    return {
+      projectName: card.projectName,
+      platform: card.platform,
+      technologies: card.technologies,
+      members: card.members,
+      progress: card.progress,
+      timeEstimated: card.timeEstimated,
+      leftTime: card.leftTime,
+      public: card.public,
+    };
+  });
+
+  const latestCard = workingCards.slice(0, 2);
+  const allCards = workingCards.slice(2, workingCards.length);
+
+  return {
+    props: {
+      workingCards,
+      revalidate: 60 * 60 * 8,
+    },
+  };
+}; */
