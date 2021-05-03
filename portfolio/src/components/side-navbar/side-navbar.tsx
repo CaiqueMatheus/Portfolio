615e70e01a76fe1512bca9e724a4eda7faf82d86
Caique Matheus <dev.caique@gmail.com>
2021-05-03 16:31:07 -0300

    Adicionado Tailwind

--- portfolio/src/components/side-navbar/side-navbar.tsx
+++ portfolio/src/components/side-navbar/side-navbar.tsx
@@ -1,26 +1,43 @@
 import styles from "./side-navbar.module.scss";
+import ActiveLink from "../../services/ActiveLink";
+import React from "react";
+import { useRouter } from "next/router";
 
 export default function sideNavbar() {
+  const router = useRouter();
   return (
     <section className={styles.sideNavbar}>
       <div className={styles.nav}>
         <ul>
-          <a>
-            <li>Home</li>
-          </a>
-          <a>
-            <li>Codes</li>
-          </a>
-          <a>
-            <li>Design</li>
-          </a>
+          <ActiveLink href="/" activeClassName="active">
+            <a>
+              <li>Home</li>
+            </a>
+          </ActiveLink>
+          <ActiveLink href="/codes/dashboard" activeClassName="active">
+            <a>
+              <li>Codes</li>
+            </a>
+          </ActiveLink>
+          <ActiveLink href="/">
+            <a>
+              <li>Design</li>
+            </a>
+          </ActiveLink>
+
           <div className={styles.spacer} />
-          <a>
-            <li>About</li>
-          </a>
-          <a>
-            <li>Contact</li>
-          </a>
+
+          <ActiveLink href="">
+            <a>
+              <li>About</li>
+            </a>
+          </ActiveLink>
+          <ActiveLink href="">
+            <a>
+              <li>Contact</li>
+            </a>
+          </ActiveLink>
+
           <div className={styles.spacer} />
         </ul>
       </div>
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  prefix: 'tw-',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+
+    extend: {
+      spacing: {
+
+      },
+      padding: {
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
