a2d8d5a85dfc39d89f6f825747f5f32599d8242c
Caique Matheus <dev.caique@gmail.com>
2021-05-12 17:29:45 -0300

    sidenav alterada para nav

--- portfolio/src/pages/_app.tsx
+++ portfolio/src/pages/_app.tsx
@@ -2,14 +2,15 @@ import "../styles/global.scss";
 import "../../tailwind.config";
 import Footer from "../components/footer";
 import React from "react";
-import SideNavbar from "../components/side-navbar";
+import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <div>
+      <Navbar />
         <main className="flex flex-row">
-          <SideNavbar />
+          
           <Component {...pageProps} />;
         </main>
         <Footer />
--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -3,10 +3,8 @@ import styles from "./home.module.scss";
 
 export default function Home() {
   return (
-    <section className={styles.container}>
-      <h1>
-        <Link href="#"> Learn </Link> Next.js!
-      </h1>
-    </section>
+    <div>
+
+    </div>
   );
 }
